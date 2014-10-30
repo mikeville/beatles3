747bb2221ab45c951fd6610b3af67b2fe514ff2e
mikeville <thedeal@gmail.com>
2014-10-30 18:24:23 -0400

    Added data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('feed me javascripts')
+var data = {
+  master: {}
+}
+
+var d3Sketch1 = function(){
+  console.log(data.master[0].albumKey);
+}
+
+
+$(function(){
+
+  // ======================================================
+  // GET DATA ================================
+  // ======================================================
+
+  // Loading message
+  $("#loading-message").html("loading data...");
+
+  // "API" call
+  d3.json("/data/dataMaster.json", function(error, json) {
+    if (error) return console.warn(error);
+    data.master = json.beatlesData;
+
+    // Clear loading message
+    $("#loading-message").empty();
+
+    d3Sketch1();
+
+
+  })
+  
+})
